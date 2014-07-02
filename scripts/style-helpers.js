d30ba1c19b5add5eb1a16351a6c2cfa4ee10e96c
JasWSInc <jason@websharks-inc.com>
2014-07-01 19:27:02 -0800

    Lack of icon/sender on certain threaded msgs. See: websharks/slackmagic#2

--- scripts/style-helpers.js
+++ scripts/style-helpers.js
@@ -2,27 +2,36 @@
 {
 	'use strict'; // Standards.
 
-	var slack = {initialized: false, timeout: 0};
-
+	var slack = {
+		initialized                : false,
+		onDOMSubtreeModifiedTimeout: 0,
+		onDOMSubtreeModifiedRunning: false
+	};
 	slack.init = function() // Initializer.
 	{
 		if(slack.initialized) return; // Already done.
 
 		$('#msgs_div').on('DOMSubtreeModified', function()
 		{
-			clearTimeout(slack.timeout), // Clear previous timeout.
-				slack.timeout = setTimeout(slack.onDOMSubtreeModified, 500);
+			if(slack.onDOMSubtreeModifiedRunning) return;
+
+			clearTimeout(slack.onDOMSubtreeModifiedTimeout), // Clear previous timeout.
+				slack.onDOMSubtreeModifiedTimeout = setTimeout(slack.onDOMSubtreeModified, 500);
 
 		}).trigger('DOMSubtreeModified');
 	};
 	slack.onDOMSubtreeModified = function()
 	{
+		slack.onDOMSubtreeModifiedRunning = true;
+
 		$('#msgs_div').find('> .message').has('> .message_sender[class*="via_HipChat"]')
 			.addClass('via-hipchat'); // Mark via HipChat for CSS styles.
+
+		slack.onDOMSubtreeModifiedRunning = false;
 	};
 	slack.initializer = function()
 	{
-		if((slack.$msg = $('textarea#message-input')).length)
+		if($.isReady && $('#msgs_div > div').length && $('textarea#message-input').length)
 			clearInterval(slack.initializerInterval), slack.init(),
 				slack.initialized = true; // All set now :-)
 	};
